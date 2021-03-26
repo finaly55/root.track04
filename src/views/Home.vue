7281402e765d6570097390c35dc230a8f35ae5f1
Alex Lri <alexandre.lairie@gmail.com>
2021-03-26 11:47:57 +0100

    update Home.vue (tableau des scores)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,103 @@
 <template>
-  <div class="home">Accueil</div>
+  <div class="home">
+    <h2>Tableau des scores</h2>
+    <br />
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="6"
+      hide-default-footer
+      class="elevation-1"
+    ></v-data-table>
+  </div>
 </template>
 
-<script>
 
+<script>
 export default {
   name: "Home",
-  components: {
-  },
-  created() {
+  components: {},
+  created() {},
+  data() {
+    return {
+      headers: [
+        {
+          text: "Exercice",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Exercice 1", value: "exercice1" },
+        { text: "Exercice 2", value: "exercice2" },
+        { text: "Exercice 3", value: "exercice3" },
+        { text: "Exercice 4", value: "exercice4" },
+        { text: "Exercice 5", value: "exercice5" },
+        { text: "Exercice 6", value: "exercice6" },
+        { text: "Total", value: "total" },
+      ],
+      desserts: [
+        {
+          name: "Task Force 01",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+        {
+          name: "Task Force 02",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+        {
+          name: "Task Force 03",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+        {
+          name: "Task Force 04",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+        {
+          name: "Task Force 05",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+        {
+          name: "Task Force 06",
+          exercice1: 159,
+          exercice2: 6.0,
+          exercice3: 24,
+          exercice4: 4.0,
+          exercice5: 22,
+          exercice6: 10,
+          total: 100,
+        },
+      ],
+    };
   },
 };
 </script>
