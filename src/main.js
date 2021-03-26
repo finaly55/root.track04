8e806bc2e74f76113ecd85119c59d0c8996a1ce5
Alex Lri <alexandre.lairie@gmail.com>
2021-03-26 11:14:00 +0100

    databaseURL

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import vuetify from './plugins/vuetify';
-import firebase from 'firebase';
-import moment from "moment"
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+import moment from "moment";
+Vue.config.productionTip = false;
 
-moment.locale('fr');
+moment.locale("fr");
 
-let app = '';
+let app = "";
 
 const config = {
     apiKey: "AIzaSyDsIHX4iNotZw8Bau_8h5gWKG6Hsbf4P78",
@@ -17,20 +17,22 @@ const config = {
     projectId: "taskforce04-7df7a",
     storageBucket: "taskforce04-7df7a.appspot.com",
     messagingSenderId: "151262402541",
-    appId: "1:151262402541:web:f077fd5d3793bd455879e4"
+    appId: "1:151262402541:web:f077fd5d3793bd455879e4",
+    databaseURL: "https://taskforce04-7df7a-default-rtdb.firebaseio.com/",
 };
 
 firebase.initializeApp(config);
 
-firebase.auth().onAuthStateChanged(async (data) => {
+// Get a reference to the database service
+var database = firebase.database();
 
-
+firebase.auth().onAuthStateChanged(async(data) => {
     if (!app) {
         /* eslint-disable no-new */
         app = new Vue({
             vuetify,
             router,
-            render: h => h(App)
-        }).$mount('#app');
+            render: (h) => h(App),
+        }).$mount("#app");
     }
-})
+});
