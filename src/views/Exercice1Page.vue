3aae0f413945f7e271f53ada9b540d354f144924
Julien Menard <julien.menard@campus.academy>
2021-03-27 16:43:40 +0100

    Mise en place système de flag
    Seulement sur l'exo 1 pour l'instant
    Permet de vérifier si la taskforce à déjà effectué l'exo
    #TrèsSale

--- src/views/Exercice1Page.vue
+++ src/views/Exercice1Page.vue
@@ -3,6 +3,11 @@
     <h2>Exercice 1</h2>
     <br />
     <br />
+    <v-alert v-if="hasDoneTheExercice === true"
+      type="warning">
+      Vous avez déjà fait cet exercice
+    </v-alert>
+
     <v-alert
       v-model="alert"
       dismissible
@@ -14,13 +19,105 @@
     >
       Contactez Ludovic Heurtin sur Teams pour obtenir vos identifiants.
     </v-alert>
+
+    <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+    >
+      <v-text-field
+          label="Flag"
+          v-model="flag"
+          :rules="flagRules"
+          style="width: 150px"
+          required>
+      </v-text-field>
+      <v-btn
+          :disabled="flag.length === 0"
+          color="success"
+          class="mr-4"
+          @click="validate"
+      >
+        Envoyer
+      </v-btn>
+    </v-form>
+
+    <v-alert v-if="isFlagGood === true"
+        type="success"
+        class="mt-5"
+       dismissible
+    >
+      Bon flag !
+    </v-alert>
+    <v-alert v-if="isFlagGood === false"
+          type="error"
+          class="mt-5"
+         dismissible
+    >
+      Mauvais flag :(
+    </v-alert>
+
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+import Vue from "vue";
+
 export default {
   name: "Exercice1Page",
-};
+  data: () => ({
+    valid: true,
+    flag : '',
+    flagRules: [
+      v => !!v || 'Le champ doit être rempli',
+      /*v => v.length <= 10 || 'Name must be less than 10 characters',*/
+    ],
+    isFlagGood : '',
+    userConnected: {},
+    hasDoneTheExercice : false
+  }),
+  mounted()
+  {
+    firebase.database().ref('taskforce/' + this.userConnected.number + '/exercice/01').once('value').then((snapshot) => {
+      if (typeof snapshot.val() !== "string")
+      {
+        this.hasDoneTheExercice = true;
+      }
+    });
+  },
+  async created() {
+    const context = this;
+    Vue.prototype.$updateUserConnected = async function (user) {
+      context.userConnected = user;
+    };
+    const currentUser = firebase.auth().currentUser;
+
+    let taskForceNb = currentUser.email.split("@");
+    taskForceNb = taskForceNb[0].substr(9);
+
+    this.userConnected = {
+      number: taskForceNb,
+    };
+  },
+  methods: {
+    validate()
+    {
+      let retrievedFlag = "";
+      if (!this.hasDoneTheExercice)
+      {
+        firebase.database().ref('flags/01').once('value').then((snapshot) => {
+          retrievedFlag = snapshot.val();
+          this.isFlagGood = this.flag === retrievedFlag;
+          let update = {};
+          update['taskforce/' + this.userConnected.number + '/exercice/01'] = 10;
+          firebase.database().ref().update(update);
+          this.hasDoneTheExercice = true;
+        });
+      }
+    },
+  },
+}
 </script>
 
 <style scoped>
