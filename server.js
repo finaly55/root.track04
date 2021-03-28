d093b0ff0fc96f1dc857a3bd03e386d15fe776d7
LEBATARD Julien <finaly55@gmail.com>
2021-03-28 23:34:21 +0200

    amélioration fichier server.js

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 var express = require('express');
-var http = require('http');
-var cors = require('cors')
+var https = require('https');
 var fs = require('fs');
 var serveStatic = require('serve-static');
-const shell = require('shelljs')
+var cors = require('cors')
 
+// https certificates
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/taskforce4.rj-it.fr/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/taskforce4.rj-it.fr/fullchain.pem')
+};
 
-// This line is from the Node.js HTTPS documentation.
-// Create a service (the app object is just a callback).
+// create a service (the app object is just a callback).
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 
-// Create an HTTPS service identical to the HTTP service.
-http.createServer(app).listen(port);
+// create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(port, () => {
+    console.log("L'APP écoute sur le port " + port);
+});
 
+
+
+// create API for execute script
 const api = express()
 
+// middleware
 api.use(cors());
 api.options('*', cors());
 
+// routes
 api.get('/new-vm/:id', (req, res) => {
     console.log(req.params.id)
     shell.exec('./scripts/' + req.params.id + '/NewVM.sh')
-    res.json({"text": "VM created"})
+    res.json({ "text": "VM created" })
 })
 
 api.get('/del-vm/:id', (req, res) => {
@@ -31,8 +41,10 @@ api.get('/del-vm/:id', (req, res) => {
     res.json({ "text": "VM deleted" })
 })
 
-api.listen(5001, () => {
-    console.log("Serveur à l'écoute")
+// run API server
+var portAPI = process.env.PORTAPI || 5001;
+
+api.listen(portAPI, () => {
+    console.log("L'API écoute sur le port " + portAPI)
 })
 
-console.log('server started ' + port);
