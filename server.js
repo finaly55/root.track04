e05c3480cb6027746bb5cac6679910c8fa97cbef
LEBATARD Julien <finaly55@gmail.com>
2021-03-28 22:05:53 +0200

    test api

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require('express');
-var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var serveStatic = require('serve-static');
+const shell = require('shelljs')
 
+ 
 // This line is from the Node.js HTTPS documentation.
-var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/taskforce4.rj-it.fr/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/taskforce4.rj-it.fr/fullchain.pem')
-};
-
 // Create a service (the app object is just a callback).
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 
 // Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(port);
+http.createServer(app).listen(port);
+
+const api = express()
+
+api.get('/parkings', (req,res) => {
+    shell.exec('./shell.bat')
+    res.send("Liste des parkings")
+})
+
+api.listen(5001, () => {
+    console.log("Serveur à l'écoute")
+})
 
 console.log('server started '+ port);
